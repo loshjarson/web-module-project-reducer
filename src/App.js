337d77243e9304b85a4793dd412c2c0447ed0315
loshjarson <jlarson129@outlook.com>
2021-08-18 16:47:04 -0400

    connected reducer and displayed state within the UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, { initialState } from './reducers/index';
+import { addOne } from './actions';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleClick = () => {
+    dispatch(addOne());
+  }
 
   return (
     <div className="App">
@@ -17,10 +24,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleClick}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
